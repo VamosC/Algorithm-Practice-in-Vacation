181b51f900720613144c935c19970e5cd9ec36d2
VamosC <3160102243@zju.edu.cn>
2019-04-01 09:02:17 +0800

    finish

---
+++ 965/VamosC.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        if(root == NULL)
+        	return true;
+        if(root->left)
+        	if(!isUnivalTree(root->left) || root->left->val != root->val)
+        		return false;
+        if(root->right)
+        	if(!isUnivalTree(root->right) || root->right->val != root->val)
+        		return false;
+        return true;
+    }
+};
