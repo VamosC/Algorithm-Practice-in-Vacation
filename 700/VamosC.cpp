8f769611c8ad6f0d8e58a3df18e0e4e3d0d59a4a
VamosC <3160102243@zju.edu.cn>
2019-04-01 08:52:30 +0800

    BST search

---
+++ 700/VamosC.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root != NULL) {
+        	if(val < root->val)
+        		root = root->left;
+        	else if(val > root->val)
+        		root = root->right;
+        	else
+        		break;
+        }
+        return root;
+    }
+};
