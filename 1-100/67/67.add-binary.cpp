cb7e2fff69c0c29ea75d9b316be33bdf7ba34df8
VamosC <3160102243@zju.edu.cn>
2019-07-21 16:12:20 +0800

    add 67

--- 1-100/67/67.add-binary.cpp
+++ 1-100/67/67.add-binary.cpp
@@ -8,10 +8,24 @@ class Solution
 public:
     string addBinary(string a, string b)
     {
-
-        vector<int> for (int i = a.size() - 1, j = b.size() - 1; i >= 0 && j >= 0; i--, j--)
+        string res;
+        auto carry = 0;
+        int i, j;
+        for (i = a.size() - 1, j = b.size() - 1; i >= 0 && j >= 0; i--, j--)
         {
-            if ()
+            carry = a[i] & b[j] & carry;
+            res.insert(0, '0' + a[i] ^ b[j] ^ carry);
         }
+        while (i >= 0)
+        {
+            carry = a[i] & carry;
+            res.insert(0, '0' + a[i] ^ carry);
+        }
+        while (j >= 0)
+        {
+            carry = b[j] & carry;
+            res.insert(0, '0' + b[j] ^ carry);
+        }
+        return res;
     }
 };
