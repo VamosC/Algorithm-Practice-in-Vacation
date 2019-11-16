4f5e431d37afbae222c47a6d52a28991a6ef7ef8
VamosC <3160102243@zju.edu.cn>
2019-11-16 14:33:11 +0800

    add 22

--- 22/22.generate-parentheses.cpp
+++ 22/22.generate-parentheses.cpp
@@ -13,6 +13,33 @@ public:
         vector<string> res;
         if (n == 0)
             return res;
+        string s;
+        generate(s, n, n, res);
+        return res;
+    }
+
+private:
+    void generate(string &s, int left_remain, int right_remain, vector<string> &res)
+    {
+        if (left_remain == 0 && right_remain == 0)
+        {
+            res.push_back(s);
+            return;
+        }
+        if (left_remain > right_remain)
+            return;
+        if (left_remain > 0)
+        {
+            s.push_back('(');
+            generate(s, left_remain - 1, right_remain, res);
+            s.pop_back();
+        }
+        if (right_remain > 0 && left_remain != right_remain)
+        {
+            s.push_back(')');
+            generate(s, left_remain, right_remain - 1, res);
+            s.pop_back();
+        }
     }
 };
 // @lc code=end
